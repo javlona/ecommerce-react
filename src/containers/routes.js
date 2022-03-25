c9d9d923607883ba4f1d129c52360905a59dcda7
Javlon <59303287+javlona@users.noreply.github.com>
2022-03-25 08:21:17 -0500

    routes finished

--- src/containers/routes.js
+++ src/containers/routes.js
@@ -1,41 +1,79 @@
 import { lazy } from "react"
 import { BiHomeAlt } from 'react-icons/bi'
+import { Navigate } from "react-router"
 
+const Home = lazy(() => import('./Home')) 
 const SignIn = lazy(() => import('./Auth/signIn')) 
 const SignUp = lazy(() => import('./Auth/signUp'))
-const Home = lazy(() => import('./Header')) 
+const About = lazy(() => import('./About')) 
+const Shop = lazy(() => import('./Shop')) 
+const Contact = lazy(() => import('./Contact')) 
+const Profile = lazy(() => import('./Profile')) 
+const Checkout = lazy(() => import('./Checkout'))
+
+const mainRoutes = [
+    {
+        path: '/',
+        element: Home,
+        allowedRoles: ['admin', 'user'],
+        title: 'Home',
+        icon: <BiHomeAlt />
+    },
+    {
+        path: 'about',
+        element: About,
+        allowedRoles: ['admin', 'user'],
+        title: 'About', 
+    },
+    {
+        path: 'shop',
+        element: Shop,
+        allowedRoles: ['admin', 'user'],
+        title: 'Shop',
+    },
+    {
+        path: 'contact',
+        element: Contact,
+        allowedRoles: ['admin', 'user'],
+        title: 'Contact',
+    },
+    {
+        path: 'checkout',
+        element: Checkout,
+        allowedRoles: ['admin', 'user'],
+        title: 'Checkout',
+    },
+    {
+        path: '*',
+        element: <Navigate to='/' replace/>,
+    },
+
+]
 
 const routes = {
     public: [
         {
-            path: 'sing-in',
+            path: 'sign-in',
             element: SignIn,
             allowedRoles: ['admin', 'user'],
-            title: 'Sing In'
+            title: 'Sign In'
         },
         {
-            path: 'sing-out',
+            path: 'sign-up',
             element: SignUp,
             allowedRoles: ['admin', 'user'],
-            title: 'Sing Out', 
+            title: 'Sign Up', 
         },
-        {
-            path: '*',
-            element: Home,
-        }
+        ...mainRoutes
     ],
     private: [
         {
-            path: '/',
-            element: Home,
+            path: 'profile',
+            element: Profile,
             allowedRoles: ['admin', 'user'],
-            title: 'Home',
-            icon: <BiHomeAlt />
+            title: 'Profile',
         },
-        {
-            path: '*',
-            element: Home,
-        }
+        ...mainRoutes
     ],
 }
 
