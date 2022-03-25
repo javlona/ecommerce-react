c928b9bccacaf5c5822a41909ee0d7bf1bd674b1
Javlon <59303287+javlona@users.noreply.github.com>
2022-03-25 08:21:00 -0500

    routes added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { Route, Routes } from 'react-router';
 import './App.css';
+import Footer from './containers/Footer';
+import Header from './containers/Header';
+import routes from './containers/routes';
 
 function App() {
-  return (
-    <div className="App">
-      Home
-    </div>
-  );
+  const user = null
+
+  if(user){
+    return (
+      <div className="App">
+        <Header />
+        <div className="content">
+          <Suspense fallback={<h4>loading...</h4>}>
+            <Routes>
+              {
+                routes.private.map((route) => {
+                  const {path, element: Component} = route
+                  return <Route key={path} path={path} element={path=='*' ? Component : <Component/>} />;
+                })
+              }
+            </Routes>
+          </Suspense>
+          <Footer />
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div className="App">
+        <Header />
+        <Suspense fallback={<h4>loading...</h4>}>
+          <Routes>
+            {
+              routes.public.map((route) => {
+                const {path, element: Component } = route
+                return <Route key={path} path={path} element={path=='*' ? Component : <Component />} />
+              })
+            }
+          </Routes>
+        </Suspense>
+        <Footer />
+      </div>
+    )
+  }
 }
 
 export default App;
