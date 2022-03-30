e197c4ef03b88c267351b7f13a9176870f56a661
Javlon <59303287+javlona@users.noreply.github.com>
2022-03-30 09:09:42 -0500

    axios and authSlicers added

---
+++ src/utils/axios.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { store } from '../store';
+
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:9000' : 'http://localhost:3000';
+
+export default axios.create({
+    baseURL,
+    withCredentials: true,
+});
+
+axios.interceptors.request.use(config => {
+    const token = store.getState().auth?.token || '';
+    config.headers.authorization = token ? `Bearer ${token}` : '';
+    return config
+})
+
+axios.interceptors.response.use(response => response, error => {
+    if (error.response.status === 401) {
+        store.dispatch(logout);
+    }
+    return Promise.reject(error);
+})
