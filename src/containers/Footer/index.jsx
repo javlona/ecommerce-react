fe0be30d6390b9eb163a0a00de3db539cb21654c
Javlon <59303287+javlona@users.noreply.github.com>
2022-04-06 12:10:33 -0500

    footer styled

--- src/containers/Footer/index.jsx
+++ src/containers/Footer/index.jsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Wrapper } from '../../style'
+import FooterSty from './style'
+import {FaFacebookF, FaLinkedinIn, FaTwitter} from 'react-icons/fa'
+import {SiInstagram} from 'react-icons/si'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <FooterSty>
+      <Wrapper>
+        <ul className="footer__links">
+          <li className="footer__links-about">
+            <h4 className='footer__title'>About us</h4>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi, tempore.</p>
+            <div className="footer__follow">
+              <h4 className='footer__title'>Follow us</h4>
+              <ul className="footer__follow-icons">
+                <Link to='/'><FaFacebookF /></Link>
+                <Link to='/'><SiInstagram /></Link>
+                <Link to='/'><FaLinkedinIn /></Link>
+                <Link to='/'><FaTwitter /></Link>
+              </ul>
+            </div>
+          </li>
+          <li className="footer__links-info">
+            <h4 className='footer__title'>Information</h4>
+            <Link to='/profile'>Profile</Link>
+            <Link to='/'>About us</Link>
+            <Link to='/'>Contact</Link>
+            <Link to='/'>Address</Link>
+            <Link to='/'>Return Policy</Link>
+          </li>
+          <li className="footer__links-returns">
+            <h4 className='footer__title'>Returns</h4>
+            <Link to='/'>FAQs</Link>
+            <Link to='/'>Size Guide</Link>
+            <Link to='/'>Support</Link>
+            <Link to='/'>Sell with us</Link>
+          </li>
+          <li className="footer__links-contact">
+            <h4 className='footer__title'>Get in touch</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae odio, eius officia quis dolor omnis.</p>
+          </li>
+        </ul>
+        <div className="footer__copyright">
+
+        </div>
+      </Wrapper>
+    </FooterSty>
   )
 }
 
---
+++ src/containers/Footer/style.js
@@ -0,0 +1,35 @@
+import sty from "styled-components";
+import color from "../../utils/color"
+
+const FooterSty = sty.div`
+    margin: 80px 0;
+    .footer__links {
+        display: flex;
+        gap: 20px;
+        text-align: left;
+    }
+    .footer__title {
+        color: ${color.title};
+        font-weight: 400;
+        font-size: 20px;
+        margin-bottom: 1rem;
+    }
+    .footer__follow-icons {
+        display: flex;
+        gap: 10px;
+        a {
+            display: inline-flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 50%; 
+            background: ${color.text};
+            padding: 5px;
+            width: 40px;
+            height: 40px;
+            font-size: 22px;
+            color: #fff;
+        }
+    }
+`
+
+export default FooterSty;
