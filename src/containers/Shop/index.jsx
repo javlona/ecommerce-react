1321b45b601faf4f55f92177183fdedaab56b7b7
Javlon <59303287+javlona@users.noreply.github.com>
2022-04-07 23:53:51 -0500

    shop page added

--- src/containers/Shop/index.jsx
+++ src/containers/Shop/index.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { Wrapper } from '../../style'
+import ShopSty from './style'
+import { Breadcrumb } from 'antd';
 
 function Shop() {
   return (
-    <div>Shop</div>
+    <ShopSty>
+      <Wrapper>
+        <Breadcrumb>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>
+            <a href="">Application Center</a>
+          </Breadcrumb.Item>
+          <Breadcrumb.Item>
+            <a href="">Application List</a>
+          </Breadcrumb.Item>
+          <Breadcrumb.Item>An Application</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="shop__content">
+          <div className='shop__category'>
+            <h4>Shop by categories</h4>
+            <ul className='shop__cats'>
+              <li>&rarr; Men</li>
+              <li>&rarr; Women</li>
+              <li>&rarr; Kids</li>
+              <li>&rarr; Trending</li>
+            </ul>
+          </div>
+        <div className='shop_cards'>
+          
+        </div>
+        </div>
+      </Wrapper>
+    </ShopSty>
   )
 }
 
---
+++ src/containers/Shop/style.js
@@ -0,0 +1,8 @@
+import sty from 'styled-components';
+import color from '../../utils/color'
+
+const ShopSty = sty.div`
+
+`
+
+export default ShopSty;
