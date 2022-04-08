1f74b904b1c351420b87cc7cfcf63efbb35459f7
Javlon <59303287+javlona@users.noreply.github.com>
2022-04-08 00:10:20 -0500

    shop page

--- src/containers/Shop/index.jsx
+++ src/containers/Shop/index.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import { Wrapper } from '../../style'
-import ShopSty from './style'
-import { Breadcrumb } from 'antd';
+import React from "react";
+import { Wrapper } from "../../style";
+import ShopSty from "./style";
+import { Breadcrumb } from "antd";
+import { Card } from "antd";
+import suit from "../../assets/suit.jpg";
+import { useSelector } from "react-redux";
+import Button from "../../components/Button";
 
 function Shop() {
+  const { Meta } = Card;
+  const user = useSelector(state => state.auth.token)
+
   return (
     <ShopSty>
       <Wrapper>
@@ -18,22 +25,89 @@ function Shop() {
           <Breadcrumb.Item>An Application</Breadcrumb.Item>
         </Breadcrumb>
         <div className="shop__content">
-          <div className='shop__category'>
+          <div className="shop__category">
             <h4>Shop by categories</h4>
-            <ul className='shop__cats'>
+            <ul className="shop__cats">
               <li>&rarr; Men</li>
               <li>&rarr; Women</li>
               <li>&rarr; Kids</li>
               <li>&rarr; Trending</li>
             </ul>
           </div>
-        <div className='shop_cards'>
-          
-        </div>
+          <div className="shop_cards">
+            <div className="shop__action">
+              {user && <Button type="secondary">Add product</Button>}
+            </div>
+            <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={
+                <img
+                  alt="example"
+                  src={suit}
+                />}>
+              <Meta
+                title="Europe Street beat"
+                description="www.instagram.com"
+              />
+            </Card>
+            <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={
+                <img
+                  alt="example"
+                  src={suit}
+                />}>
+              <Meta
+                title="Europe Street beat"
+                description="www.instagram.com"
+              />
+            </Card>
+            <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={
+                <img
+                  alt="example"
+                  src={suit}
+                />}>
+              <Meta
+                title="Europe Street beat"
+                description="www.instagram.com"
+              />
+            </Card>
+            <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={
+                <img
+                  alt="example"
+                  src={suit}
+                />}>
+              <Meta
+                title="Europe Street beat"
+                description="www.instagram.com"
+              />
+            </Card>
+            <Card
+              hoverable
+              style={{ width: 240 }}
+              cover={
+                <img
+                  alt="example"
+                  src={suit}
+                />}>
+              <Meta
+                title="Europe Street beat"
+                description="www.instagram.com"
+              />
+            </Card>
+          </div>
         </div>
       </Wrapper>
     </ShopSty>
-  )
+  );
 }
 
-export default Shop
+export default Shop;
--- src/containers/Shop/style.js
+++ src/containers/Shop/style.js
@@ -2,7 +2,18 @@ import sty from 'styled-components';
 import color from '../../utils/color'
 
 const ShopSty = sty.div`
-
+    .shop__content {
+        display: flex;
+    }
+    .shop__category {
+        text-align: left;
+    }
+    .shop_cards {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        gap: 20px;
+    }
 `
 
 export default ShopSty;
