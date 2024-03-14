9a2b797bc1ed9b0c4021dff5a17ee150c8133605
Javlon <59303287+javlona@users.noreply.github.com>
2024-03-13 23:13:47 -0500

    ..

--- src/containers/Header/index.jsx
+++ src/containers/Header/index.jsx
@@ -1,8 +1,8 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { BiSearch, BiCartAlt } from "react-icons/bi";
-import { CgProfile } from 'react-icons/cg'
-import { Menu, Dropdown, Button as Btn, Drawer } from 'antd';
+import { CgProfile } from "react-icons/cg";
+import { Menu, Dropdown, Button as Btn, Drawer } from "antd";
 import Button from "../../components/Button";
 import HeaderSty from "./style";
 import Input from "../../components/Input";
@@ -13,7 +13,7 @@ import axios from "../../utils/axios";
 import { searchReducer } from "../../store/products";
 
 function Header() {
-  const user = useSelector(state => state.auth.token);
+  const user = useSelector((state) => state.auth.token);
   const products = useSelector((state) => state.products);
 
   const cartItems = 2;
@@ -22,14 +22,16 @@ function Header() {
 
   const dispatch = useDispatch();
 
-  const searchQueryHandler = (e) => { setQuery(e.target.value) }
+  const searchQueryHandler = (e) => {
+    setQuery(e.target.value);
+  };
 
   //call redux function
   const searchHandler = (e) => {
     e.preventDefault();
-    dispatch(searchReducer(query))
-    console.log(products)
-  }
+    dispatch(searchReducer(query));
+    console.log(products);
+  };
 
   const logoutHandler = () => {
     dispatch(logout());
@@ -55,18 +57,25 @@ function Header() {
 
   return (
     <HeaderSty>
-      <Drawer title="Basic Drawer" placement="right" onClose={onClose} visible={visible}>
+      <Drawer
+        title="Basic Drawer"
+        placement="right"
+        onClose={onClose}
+        visible={visible}
+      >
         <p>Some contents...</p>
         <p>Some contents...</p>
         <p>Some contents...</p>
       </Drawer>
       <div className="header__top">
-        <Wrapper display='flex' jc='space-between' ai='center'>
+        <Wrapper display="flex" jc="space-between" ai="center">
           <h3 className="header__top-msg">Free Delivery</h3>
           <div className="header__top-links">
             {user ? (
               <Dropdown overlay={menuProfile} placement="bottom" arrow>
-                <Btn><CgProfile /></Btn>
+                <Btn>
+                  <CgProfile />
+                </Btn>
               </Dropdown>
             ) : (
               <Link to={"/sign-in"}>Sign in</Link>
@@ -77,40 +86,40 @@ function Header() {
         </Wrapper>
       </div>
       <div className="header__nav">
-        <Wrapper display='flex' jc='space-between' ai='center'>
-        <div className="header__nav-logo"><Link to='/'>online store</Link></div>
-        <div className="header__nav-links">
-          <Link to="/">HOME</Link>
-          <Link to="/about">ABOUT US</Link>
-          <Link to="/shop">SHOP</Link>
-          <Link to="/contact">CONTACT</Link>
-        </div>
-        <div className="header__nav-actions">
-          <form onSubmit={ searchHandler }>
-            <Input
-              className="header__input"
-              onChange={ searchQueryHandler }
-              type="search"
-              name="search"
-              placeholder="Search"
+        <Wrapper display="flex" jc="space-between" ai="center">
+          <div className="header__nav-logo">
+            <Link to="/">online store</Link>
+          </div>
+          <div className="header__nav-links">
+            <Link to="/">HOME</Link>
+            <Link to="/about">ABOUT US</Link>
+            <Link to="/shop">SHOP</Link>
+            <Link to="/contact">CONTACT</Link>
+          </div>
+          <div className="header__nav-actions">
+            <form onSubmit={searchHandler}>
+              <Input
+                className="header__input"
+                onChange={searchQueryHandler}
+                type="search"
+                name="search"
+                placeholder="Search"
+              />
+              <button className="header__search-button">
+                <BiSearch />
+              </button>
+              <Dropdown trigger={["click"]}>
+                <a onClick={(e) => e.preventDefault()}>click me</a>
+              </Dropdown>
+            </form>
+            <Button
+              title="item(s)"
+              type="secondary"
+              icon={<BiCartAlt />}
+              data={cartItems}
+              onClick={showDrawer}
             />
-            <button className="header__search-button">
-              <BiSearch />
-            </button>
-            <Dropdown overlay={menu} trigger={['click']}>
-              <a onClick={e => e.preventDefault()}>
-                click me
-              </a>
-            </Dropdown>
-          </form>
-          <Button
-            title="item(s)"
-            type="secondary"
-            icon={<BiCartAlt />}
-            data={cartItems}
-            onClick={showDrawer}
-          />
-        </div>
+          </div>
         </Wrapper>
       </div>
     </HeaderSty>
